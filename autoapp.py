6aa57d5d4101aec0034d3ab3e5c21628f827f62a
Chema <josemaria.lizano@lynxanalytics.com>
2019-01-09 19:12:31 +0100

    added explanation on db copy to tmp

--- autoapp.py
+++ autoapp.py
@@ -2,6 +2,7 @@
 """Create an application instance."""
 import shutil
 
+# Extreme hack to overcome aws lambda read only filesystem
 shutil.copy("safu/safu.db", "/tmp/safu.db")
 
 from safu.app import create_app
