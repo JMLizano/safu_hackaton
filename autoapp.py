6ddac81b079dee888ed92a5c5e81fa8f4ae5648a
Chema <josemaria.lizano@lynxanalytics.com>
2019-01-19 13:09:55 +0100

    Downgraded to python3.5 to match gcloud compute engine

--- autoapp.py
+++ autoapp.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
-import shutil
+# import shutil
 
-# Extreme hack to overcome aws lambda read only filesystem
-shutil.copy("safu/safu.db", "/tmp/safu.db")
+# # Extreme hack to overcome aws lambda read only filesystem
+# shutil.copy("safu/safu.db", "/tmp/safu.db")
 
 from safu.app import create_app
 
